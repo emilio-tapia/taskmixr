b5ebdc7621bdb7a066b726c8d87d2680d9d3dd2f
Emilio Tapia <emta.develop@gmail.com>
2024-04-27 05:17:46 -0500

    feat(i18n): remove astro-i18next && add astro built-in i18n

---
+++ src/i18n/ui.ts
@@ -0,0 +1,514 @@
+export const languages = {
+  es: {
+    es: "Español",
+    en: "Spanish",
+    fr: "Espagnol",
+  },
+  en: {
+    es: "Inglés",
+    en: "English",
+    fr: "Anglais",
+  },
+  fr: {
+    es: "Francés",
+    en: "French",
+    fr: "Français",
+  },
+};
+
+export const defaultLang = "es";
+
+export const ui = {
+  es: {
+    menu: {
+      about: "Acerca de mi",
+      study: "Estudios",
+      experience: "Experiencia",
+      skill: "Habilidades",
+      project: "Proyectos",
+      miniproject: "Miniproyectos",
+      blog: "Blog",
+      media: "Media",
+      contact: "Contacto",
+    },
+    nav: {
+      lang: "Idioma",
+    },
+    hero: {
+      description: {
+        job1: "Diseño UI/UX",
+        job2: "Desarollo Front-end",
+        job3: "Producción Audiovisual",
+      },
+    },
+
+    aboutMe: {
+      saludo: {
+        "1": "Hola...",
+        "2": "Que gusto tenerte aquí",
+      },
+      description: {
+        "1": "Me dedico al desarrollo front-end, diseño UI/UX y producción audiovisual.",
+        "2": "Los principales aspectos de mi carrera profesional son el arte y la tecnología",
+        "3": "Mi pasión recae en el interés por descubrir nuevas posibilidades sobre la interactividad.",
+        "4": "Como productor musical, tenia un gran interés sobre el tema audiovisual / interactivo, era mi deseo poder explorar más en ese campo. Comencé a experimentar el diseño gráfico en 2018.",
+        "5": "Un día trabajando en una animación en After Effects, encontré una herramienta llamada 'expressions'. Se basaba en controlar el comportamiento de la animación por código. Realmente me gustó.",
+        "6": "Entonces empecé aprender a programar.",
+        "7": "Mi meta es seguir aprendiendo constantemente.",
+      },
+      about: [
+        "Dejame contarte un poco de mi...",
+        "",
+        "Actualmente tengo 28 años.",
+        "Vivo en Quito, Ecuador.",
+        "",
+        "Tengo experiencia en el desarrollo front-end, diseño de interfaces, implementación de estrategias de UX, producción audiovisual y sonido.",
+
+        "Me gusta aprender de la tecnología de vanguardia (librerías, frameworks, herramientas). ",
+        // "",
+        // "",
+        // "",
+        // "Tengo una fascinacion increible por la interactividad.",
+        // "",
+        // "Este proceso de comunicación entre humanos y computadoras/maquinas puede reflejarse como un proceso cognitivo al que un usuario percibe una accion de forma subjetiva. Mientras la accion pueda conectarse con la expectativa del usuario, se logrará la esperada interactividad.",
+        "",
+        "Mi meta es seguir aprendiendo e implementar interactividad en proyectos con una visión vanguardista.",
+      ],
+    },
+
+    study: {
+      "1": {
+        year: "2014-2018",
+        college: "Universidad San Francisco de Quito",
+        bachelor: "Licenciatura en Producción Musical y Sonido",
+      },
+    },
+
+    experience: {
+      "1": {
+        year: "2022-Present",
+        jobName: "Comtelec Cia. Ltda.",
+        roles: {
+          "1": "Implementeción front-end y diseño UI/UX de la página web",
+          "2": "Implementacion de sistema visual corporativo.",
+          "3": "Diseñador gráfico y branding de la compañia.",
+          "4": "Operador de máquinas de corte laser.",
+        },
+      },
+      "2": {
+        year: "2021-Present",
+        jobName: "House of Modular Synth",
+        roles: {
+          "1": "Fundador del proyecto",
+          "2": "Diseño y construcción del espacio de composición musical 'The Playroom'.",
+          "3": "Tareas de logística y administración",
+        },
+      },
+      "3": {
+        year: "2018-2019",
+        jobName: "Fundación Teatro Sucre",
+        roles: {
+          "1": "Pasante del departamento de sonido.",
+          "2": "Asistente de logística para eventos que requieran sonido en vivo.",
+        },
+      },
+    },
+
+    skills: {
+      professional: {
+        name: "Habilidades profesionales",
+        group: {
+          "1": "Desarrollador Front-End",
+          "2": "Sistema de diseño UI",
+          "3": "Desarrollo de la experiencia del usuario",
+          "4": "Diseño de animaciones web",
+          "5": "Diseño de sonido",
+          "6": "Producción audiovisual",
+        },
+      },
+      soft: {
+        name: "Aptitudes",
+        group: {
+          "1": "Trabajo bajo presión.",
+          "2": "Autodidacta.",
+          "3": "Trabajo colectivo.",
+        },
+      },
+    },
+
+    projects: {
+      comtelec: {
+        year: "2002-2023",
+        name: "Comtelec Web Page",
+        description: "Empresa de fibra optica / telecomunicaciones",
+      },
+      homs: {
+        year: "2021-2023",
+        name: "House of Modular Synth",
+        description: "Experiencia eurorack al alcance de todos",
+      },
+      taskmixr: {
+        year: "2023",
+        name: "TaskMixr",
+        description: "Aplicacion para distribuir de tareas al azar.",
+      },
+    },
+
+    footer: {
+      goodbye: ["Adiós...", "Gracias por tu visita"],
+      nav: {
+        "1": {
+          name: "Contacto",
+          emailMe: "Enviame un email",
+          cv: "Descargar CV",
+        },
+        "2": {
+          name: "Proyectos",
+        },
+        "3": {
+          name: "Redes Sociales",
+        },
+      },
+    },
+
+    extra: {
+      visitWeb: "Visitar Web",
+      takeLook: "Un poco de mi..",
+      aboutMy: "Sobre mis..",
+      spareTime: "Mi tiempo libre",
+    },
+
+    cv: {
+      location: "/resources/cv-emilio-tapia-es.pdf",
+    },
+  },
+  en: {
+    menu: {
+      about: "About me",
+      study: "Studies",
+      experience: "Experience",
+      skill: "Skills",
+      project: "Projects",
+      miniproject: "Miniprojects",
+      blog: "Blog",
+      media: "Media",
+      contact: "Contact",
+    },
+    nav: {
+      lang: "Language",
+    },
+    hero: {
+      description: {
+        job1: "UI/UX Design",
+        job2: "Front-end Developer",
+        job3: "Audiovisual Production",
+      },
+    },
+    aboutMe: {
+      saludo: {
+        "1": "Hello",
+        "2": "How are you?",
+      },
+      description: {
+        "1": "I am a front-end developer, UX/UI designer & audiovisual producer.",
+        "2": "The main aspects in my professional career are the art and technology",
+        "3": "My passion relies on the interest to discover new possibilities about being interactive.",
+        "4": "As a music producer, the audiovisual / interactive subject got my profound interest, I wanted to get involved more in that field and started to dig into graphic design theory in 2018.",
+        "5": "One day I was working on an animation in After Effects. I found something called 'expressions'. It was like controlling the animation behavior by code and I really liked it.",
+        "6": "I started to learn front-end programming in 2020.",
+        "7": "Since then I keep learning new things.",
+      },
+      about: [
+        "Let me tell you a little about me...",
+        "",
+        "I am currently 28 years old.",
+        "I live in Quito, Ecuador.",
+        "",
+        "I have experience in front-end development, interface design, implementation of UX strategies, audiovisual and sound production.",
+
+        "",
+        "I like to learn from cutting-edge technology (libraries, frameworks, tools).",
+        // "",
+        // "I have an incredible fascination with interactivity.",
+        // "",
+        // "This communication process between humans and computers/machines can be reflected as a cognitive process in which a user perceives an action subjectively. As long as the action can connect with the user's expectation, the expected interactivity will be achieved.",
+        "",
+        "My goal is to continue learning and implement interactivity in projects with an avant-garde vision.",
+      ],
+    },
+
+    study: {
+      "1": {
+        year: "2014-2018",
+        college: "Universidad San Francisco de Quito",
+        bachelor: "Bachelor in Musical Production and Sound",
+      },
+    },
+
+    experience: {
+      "1": {
+        year: "2022-Present",
+        jobName: "Comtelec Cia. Ltda.",
+        roles: {
+          "1": "UI/UX and front-end implementation of the company webpage.",
+          "2": "Development of design system.",
+          "3": "In-house design and branding.",
+          "4": "Operator of laser cutting machines.",
+        },
+      },
+      "2": {
+        year: "2021-Present",
+        jobName: "House of Modular Synth",
+        roles: {
+          "1": "Founder of the project.",
+          "2": "Design and construction of 'The Playroom'. A music studio dedicated to modular synthesizers.",
+          "3": "Administration and logistics tasks.",
+        },
+      },
+      "3": {
+        year: "2018-2019",
+        jobName: "Fundación Teatro Sucre",
+        roles: {
+          "1": "Intern of the sound department.",
+          "2": "Logistics assistant for live events in the sound area.",
+        },
+      },
+    },
+
+    skills: {
+      professional: {
+        name: "Professional Skills",
+        group: {
+          "1": "Front-end development",
+          "2": "UI System Design",
+          "3": "UX Empathy Maps",
+          "4": "Web Animation Design",
+          "5": "Sound Design",
+          "6": "Audiovisual Production",
+        },
+      },
+      soft: {
+        name: "Soft Skills",
+        group: {
+          "1": "Work under pressure",
+          "2": "Self-taught person",
+          "3": "Collective work",
+        },
+      },
+    },
+
+    projects: {
+      comtelec: {
+        year: "2002-2023",
+        name: "Comtelec Web Page",
+        description: "Telecomunications and optical fiber business",
+      },
+      homs: {
+        year: "2021-2023",
+        name: "House of Modular Synth",
+        description: "Eurorack experience for everyone",
+      },
+      taskmixr: {
+        year: "2023",
+        name: "TaskMixr",
+        description: "App to distribute tasks randomly.",
+      },
+    },
+
+    footer: {
+      goodbye: {
+        "0": "Bye... ",
+        "1": "Thanks for your visit",
+      },
+      nav: {
+        "1": {
+          name: "Contact",
+          emailMe: "Email me",
+          cv: "Download CV",
+        },
+        "2": {
+          name: "Projects",
+        },
+        "3": {
+          name: "Social Media",
+        },
+      },
+    },
+
+    extra: {
+      visitWeb: "Visit Web",
+      takeLook: "A look of my..",
+      aboutMy: "About my..",
+      spareTime: "My spare time",
+    },
+
+    cv: {
+      location: "/resources/cv-emilio-tapia-en.pdf",
+    },
+  },
+  fr: {
+    menu: {
+      about: "À propos de moi",
+      study: "Études",
+      experience: "Expérience",
+      skill: "Compétences",
+      project: "Projets",
+      miniproject: "Miniprojets",
+      blog: "Blog",
+      media: "Médias",
+      contact: "Contact",
+    },
+    nav: {
+      lang: "Langage",
+    },
+    hero: {
+      description: {
+        job1: "UI/UX Design",
+        job2: "Développement Front-end",
+        job3: "Production Audiovisuelle",
+      },
+    },
+    aboutMe: {
+      saludo: {
+        "1": "Hello",
+        "2": "How are you?",
+      },
+      description: {
+        "1": "I am a front-end developer, UX/UI designer & audiovisual producer.",
+        "2": "The main aspects in my professional career are the art and technology",
+        "3": "My passion relies on the interest to discover new possibilities about being interactive.",
+        "4": "As a music producer, the audiovisual / interactive subject got my profound interest, I wanted to get involved more in that field and started to dig into graphic design theory in 2018.",
+        "5": "One day I was working on an animation in After Effects. I found something called 'expressions'. It was like controlling the animation behavior by code and I really liked it.",
+        "6": "I started to learn front-end programming in 2020.",
+        "7": "Since then I keep learning new things.",
+      },
+      about: [
+        "Laissez-moi vous parler un peu de moi...",
+        "",
+        "J'ai actuellement 28 ans.",
+        "J'habite à Quito, en Équateur.",
+        "",
+        "J'ai de l'expérience en développement front-end, conception d'interfaces, mise en œuvre de stratégies UX, production audiovisuelle et sonore.",
+
+        "",
+        "J'aime apprendre des technologies de pointe (bibliothèques, frameworks, outils).",
+        // "",
+        // "",
+        // "J'ai une incroyable fascination pour l'interactivité.",
+        // "",
+        // "Ce processus de communication entre les humains et les ordinateurs/machines peut être reflété comme un processus cognitif dans lequel un utilisateur perçoit subjectivement une action. Tant que l'action peut se connecter avec les attentes de l'utilisateur, l'interactivité attendue sera atteinte.",
+        "",
+        "Mon objectif est de continuer à apprendre et à mettre en œuvre l'interactivité dans des projets avec une vision avant-gardiste.",
+      ],
+    },
+
+    study: {
+      "1": {
+        year: "2014-2018",
+        college: "Universidad San Francisco de Quito",
+        bachelor: "Baccalauréat en production musicale et son",
+      },
+    },
+
+    experience: {
+      "1": {
+        year: "2022-Present",
+        jobName: "Comtelec Cia. Ltda.",
+        roles: {
+          "1": "Implémentation UI/UX et front-end de la page Web de l'entreprise.",
+          "2": "Développement du système de conception.",
+          "3": "Conception et image de marque en interne.",
+          "4": "Opérateur de machines de découpe laser.",
+        },
+      },
+      "2": {
+        year: "2021-Present",
+        jobName: "House of Modular Synth",
+        roles: {
+          "1": "Fondateur du projet.",
+          "2": "Conception et construction de 'The Playroom'. Un studio de musique dédié aux synthétiseurs modulaires.",
+          "3": "Tâches administratives et logistiques.",
+        },
+      },
+      "3": {
+        year: "2018-2019",
+        jobName: "Fundación Teatro Sucre",
+        roles: {
+          "1": "Stagiaire du département son.",
+          "2": "Assistant logistique pour les événements en direct dans la zone sonore.",
+        },
+      },
+    },
+
+    skills: {
+      professional: {
+        name: "Compétences Professionnelles",
+        group: {
+          "1": "Développement front-end",
+          "2": "Conception du système d'interface utilisateur",
+          "3": "Cartes d'empathie UX",
+          "4": "Conception d'animations Web",
+          "5": "Conception sonore",
+          "6": "Production audiovisuelle",
+        },
+      },
+      soft: {
+        name: "Compétences Générales",
+        group: {
+          "1": "Travailler sous pression",
+          "2": "Autodidacte",
+          "3": "Travail collectif",
+        },
+      },
+    },
+
+    projects: {
+      comtelec: {
+        year: "2002-2023",
+        name: "Comtelec Web Page",
+        description: "Activité de télécommunications et de fibre optique",
+      },
+      homs: {
+        year: "2021-2023",
+        name: "House of Modular Synth",
+        description: "L'expérience Eurorack pour tous",
+      },
+      taskmixr: {
+        year: "2023",
+        name: "TaskMixr",
+        description: "App pour répartir les tâches de manière aléatoire",
+      },
+    },
+
+    footer: {
+      goodbye: {
+        "0": "Merci",
+        "1": "Pour votre visite",
+      },
+      nav: {
+        "1": {
+          name: "Contact",
+          emailMe: "Envoyez-moi un e-mail",
+          CV: "Télécharger CV",
+        },
+        "2": {
+          name: "Projets",
+        },
+        "3": {
+          name: "Réseaux sociaux",
+        },
+      },
+    },
+
+    extra: {
+      visitWeb: "Visit Web",
+      takeLook: "A look of my..",
+      aboutMy: "About my..",
+      spareTime: "My spare time",
+    },
+
+    cv: {
+      location: "/resources/cv-emilio-tapia-en.pdf",
+    },
+  },
+} as const;
